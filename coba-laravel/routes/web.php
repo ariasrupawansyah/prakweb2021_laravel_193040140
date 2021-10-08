4fdbeba4859d1daebc5ef50427a5c92c790e4214
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-08 13:05:41 +0700

    Mempelajari struktur folder, routes, dan view pada laravel 8 (menyelesaikan video 3)

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Aria Rupawansyah",
+        "email" => "193040140.aria@mail.unpas.ac.id",
+        "image" => "aria.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
+});
+
+// Route::get('/about', function () {
+//     return view('welcome');
+// });
