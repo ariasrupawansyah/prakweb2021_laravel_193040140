f457e8da21a21209d3f4f0a61a61012fd9b70c3f
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-22 00:53:21 +0700

    Mempelajari mengenai Database Seeder pada Laravel 8 (Menyelesaikan video ke 8)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Aria Rupawansyah',
+            'email' => '193040140.aria@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Prettier Lynn',
+            'email' => 'prettier@mail.unpas.ac.id',
+            'password' => bcrypt('12345')
+        ]);
+
+         Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+         Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+         Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum. dignissimos error ut placeat earum atque est minus culpa assumenda mollitia blanditiis excepturi autem magni veniam odio dolores! Quasi tempora laboriosam eligendi magni, reprehenderit velit itaque, voluptates suscipit aperiam eum, quam facilis eos mollitia dignissimos cupiditate consectetur. Debitis qui id ratione. Nulla nisi esse atque voluptas saepe, illum quo impedit perferendis ipsa eaque non iure maxime nemo unde inventore ratione repellendus dolorum reprehenderit eum sapiente labore! Asperiores, sit. Voluptatem omnis temporibus enim consequuntur unde, quas beatae culpa. Ex assumenda in ipsa sequi!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+         Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum. dignissimos error ut placeat earum atque est minus culpa assumenda mollitia blanditiis excepturi autem magni veniam odio dolores! Quasi tempora laboriosam eligendi magni, reprehenderit velit itaque, voluptates suscipit aperiam eum, quam facilis eos mollitia dignissimos cupiditate consectetur. Debitis qui id ratione. Nulla nisi esse atque voluptas saepe, illum quo impedit perferendis ipsa eaque non iure maxime nemo unde inventore ratione repellendus dolorum reprehenderit eum sapiente labore! Asperiores, sit. Voluptatem omnis temporibus enim consequuntur unde, quas beatae culpa. Ex assumenda in ipsa sequi!',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+         Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ke-tiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum. dignissimos error ut placeat earum atque est minus culpa assumenda mollitia blanditiis excepturi autem magni veniam odio dolores! Quasi tempora laboriosam eligendi magni, reprehenderit velit itaque, voluptates suscipit aperiam eum, quam facilis eos mollitia dignissimos cupiditate consectetur. Debitis qui id ratione. Nulla nisi esse atque voluptas saepe, illum quo impedit perferendis ipsa eaque non iure maxime nemo unde inventore ratione repellendus dolorum reprehenderit eum sapiente labore! Asperiores, sit. Voluptatem omnis temporibus enim consequuntur unde, quas beatae culpa. Ex assumenda in ipsa sequi!',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt sapiente alias repellendus iusto odit voluptatibus possimus minima rerum. dignissimos error ut placeat earum atque est minus culpa assumenda mollitia blanditiis excepturi autem magni veniam odio dolores! Quasi tempora laboriosam eligendi magni, reprehenderit velit itaque, voluptates suscipit aperiam eum, quam facilis eos mollitia dignissimos cupiditate consectetur. Debitis qui id ratione. Nulla nisi esse atque voluptas saepe, illum quo impedit perferendis ipsa eaque non iure maxime nemo unde inventore ratione repellendus dolorum reprehenderit eum sapiente labore! Asperiores, sit. Voluptatem omnis temporibus enim consequuntur unde, quas beatae culpa. Ex assumenda in ipsa sequi!',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
