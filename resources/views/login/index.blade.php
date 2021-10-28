196546f17f9c27afd24974a97b6b553d53c28734
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-29 01:32:07 +0700

    Membuat view login dan register serta pembuatan controllernya pada laravel 8 (Menyelesaikan video 14)

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.main')
+
+@section('container')
+    
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <main class="form-signin">
+            <h1 class="h3 mb-3 fw-normal text-center mt-5">Please Login</h1>
+            <i class="bi bi-person-circle d-block text-center display-1"></i>
+            <form>
+                <div class="form-floating mb-3 mt-3">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
+                </div>
+                <button class="w-100 btn btn-lg btn-danger" type="submit">Login</button>
+            </form>
+            <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
+        </main>
+    </div>
+</div>
+
+@endsection
