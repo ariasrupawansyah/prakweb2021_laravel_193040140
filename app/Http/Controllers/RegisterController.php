e7003176097b40ae2332b797358cb3d256a62c51
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-10-29 10:11:44 +0700

    Membuat fitur user registration pada laravel 8 dan bootstrap (Menyelesaikan video 15)

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,4 +15,26 @@ class RegisterController extends Controller
             'active' => 'register'
         ]);
     }
+
+    public function store(Request $request)
+    {
+        // return $request->all();
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => ['required', 'min:3', 'max:255', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        //$validatedData['password'] = bcrypt($validatedData['password']);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash('Success', 'Registration successfull! Please login');
+
+        return redirect('/login')->with('Success', 'Registration successfull! Please login');
+    }
+
 }
