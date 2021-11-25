a6735b97fa184b80af7720f05e2f606ef7f9fd57
ariasrupawansyah <193040140.aria@mail.unpas.ac.id>
2021-11-25 21:11:57 +0700

    Menambahkan fitur preview, update, & delete image di laravel 8 (Menyelesaikan video ke 22)

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Cviebrock\EloquentSluggable\Services\SlugService;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -62,7 +63,7 @@ class DashboardPostController extends Controller
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200, '...');
 
         Post::create($validatedData);
-        
+
         return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
@@ -105,6 +106,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
 
@@ -113,6 +115,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if( $request->file('image') ){
+            if($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200, '...');
@@ -131,6 +140,13 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+
+        //Melakukan delete file
+        if($post->image) {
+            Storage::delete($post->image);
+        }
+
+        //Delete table
         Post::destroy($post->id);
 
         return redirect('/dashboard/posts')->with('success', 'Post Has been Deleted!');
